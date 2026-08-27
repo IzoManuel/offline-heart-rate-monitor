e17eb35ea18d6ef6fe7cceea07a55b01e4211751
IzoManuel <israel7manuel@gmail.com>
2026-08-27 05:44:35 +0300

    Align overview metric cards consistently

--- src/components/HRDisplay.jsx
+++ src/components/HRDisplay.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function HRDisplay({ currentHR }) {
   return (
     <div className="hr-display">
+      <div className="metric-card-heading">Heart Rate</div>
       <div className="hr-value-container">
         <div className="hr-value">{currentHR}</div>
         <div className="hr-unit">BPM</div>
